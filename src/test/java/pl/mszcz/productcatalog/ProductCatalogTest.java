89257579a7374cc1e81ac4b19a524a24ac828a2d
Michał Szczurek <michalszczurek794@gmail.com>
2022-03-28 17:21:19 +0200

    more tests for productCatalog class

--- src/test/java/pl/mszcz/productcatalog/ProductCatalogTest.java
+++ src/test/java/pl/mszcz/productcatalog/ProductCatalogTest.java
@@ -9,26 +9,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductCatalogTest {
     @Test
-    void itAllowsToListProducts() {
+    void itAllowsToListOnlyPublishedProducts() {
         ProductCatalog catalog = thereIsProductCatalog();
         List<ProductData> products = catalog.getPublishedProducts();
-        assertEquals(products.size(), 0);
+        assertEquals(0, products.size());
     }
 
     @Test
-    void itAllowsToPublishProduct() {
+    void itAllowsToAddProductDraft() {
         ProductCatalog catalog = thereIsProductCatalog();
-        catalog.addProduct("1", "aaa");
-        catalog.setImageUrl("1", "fsdfhfds");
-        catalog.setPrice("1", BigDecimal.valueOf(12.50));
-
-        assertDoesNotThrow(()->{
-            catalog.publishProduct("1");
-        });
+        String productId = catalog.addProduct("lego-set-1", "super lego set");
 
         List<ProductData> products = catalog.getPublishedProducts();
-
-        assertEquals(products.size(), 1);
+        assertEquals(0, products.size());
+        assertDoesNotThrow(()->{
+            catalog.getProductById(productId);
+        });
     }
 
     @Test
@@ -51,6 +47,22 @@ public class ProductCatalogTest {
         assertThrows(CantPublishProductException.class, ()->{
             catalog.publishProduct("1");
         });
+    }
+
+    @Test
+    void itAllowsToPublishProduct() {
+        ProductCatalog catalog = thereIsProductCatalog();
+        catalog.addProduct("1", "aaa");
+        catalog.setImageUrl("1", "http://fsdfhfds.com/pr1.png");
+        catalog.setPrice("1", BigDecimal.valueOf(12.50));
+
+        assertDoesNotThrow(()->{
+            catalog.publishProduct("1");
+        });
+
+        List<ProductData> products = catalog.getPublishedProducts();
+
+        assertEquals(1, products.size());
     }
 
     private ProductCatalog thereIsProductCatalog() {
