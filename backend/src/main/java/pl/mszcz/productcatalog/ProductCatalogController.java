7d1974e595628e6b50aa2d18e7833e920b27f6c6
Michał Szczurek <michalszczurek794@gmail.com>
2022-05-22 22:35:21 +0200

    endpoint for retrieving a single product by id

--- backend/src/main/java/pl/mszcz/productcatalog/ProductCatalogController.java
+++ backend/src/main/java/pl/mszcz/productcatalog/ProductCatalogController.java
@@ -1,7 +1,10 @@
 package pl.mszcz.productcatalog;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -16,5 +19,11 @@ public class ProductCatalogController {
     @GetMapping("/api/products")
     List<ProductData> products() {
         return this.catalog.getPublishedProducts();
+    }
+
+    @GetMapping("/api/products/{id}")
+    ProductData product(@PathVariable Long id) {
+        return this.catalog.getProductById(id)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
