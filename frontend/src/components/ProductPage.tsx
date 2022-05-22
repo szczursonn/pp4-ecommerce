f16675a0bc164cd15f1d9c7807a85d91b7e3b306
Michał Szczurek <michalszczurek794@gmail.com>
2022-05-22 22:34:08 +0200

    product page init

--- frontend/src/components/ProductPage.tsx
+++ frontend/src/components/ProductPage.tsx
@@ -1,12 +1,56 @@
-import { useState } from "react"
+import React, { useState } from "react"
+import { useQuery } from "react-query"
 import { useParams } from "react-router-dom"
+import { ProductData, validateProduct } from "../types/ProductData"
 
 export const ProductPage = () => {
 
     const [quantity, setQuantity] = useState(1)
     const { id } = useParams()
 
+    const { data: product, error, isLoading } = useQuery(`product-${id}`, async () => {
+            const res = await fetch(`http://localhost:8080/api/products/${id}`)
+            if (!res.ok) throw new TypeError(res.statusText)
+            const data = await res.json()
+            if (!validateProduct(data)) throw new Error('Response validation failed')
+            return data as ProductData
+        }, {
+            staleTime: Infinity
+        }
+    )
+
+    const addToCart = () => {
+        fetch(`http://localhost:8080/api/sales/offer/${id}?quantity=${quantity}`, {
+            method: 'POST'
+        }).catch(()=>{})
+    }
+
+    const updateQuantity = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.currentTarget.valueAsNumber
+        if (value < 1 || value > 99) return
+        setQuantity(value)
+    }
+
     return <div>
-        Placeholder, id: {id}
+        <p>Placeholder, id: {id}</p>
+        {
+            isLoading ? 
+                <p>loading...</p>
+            :
+                <>
+                    {
+                    error ?
+                        <p>Error loading product</p>
+                    :
+                        <div>
+                            <img src={product!.imageUrl!} alt={product!.name}/>
+                            <p>{product!.name}</p>
+                            <p>{product!.price?.toFixed(2)} zł</p>
+                            <input type='number' min={1} max={99} value={quantity} autoComplete='off' onChange={updateQuantity}></input>
+                            <button onClick={addToCart}>add to cart</button>
+                        </div>
+                    }
+                </>
+        }
     </div>
 }
