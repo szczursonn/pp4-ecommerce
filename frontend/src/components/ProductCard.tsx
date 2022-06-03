bc92055527d28caeaee0f1c61263d51e8fbd8acc
Michał Szczurek <michalszczurek794@gmail.com>
2022-06-03 15:53:57 +0200

    loading spinner on image

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -2,17 +2,26 @@ import './ProductCard.scss'
 import { ProductData } from "../types/ProductData"
 import { useNavigate } from 'react-router-dom'
 import Price from './Price'
+import { useState } from 'react'
 
 const FALLBACK_IMG = '/logo512.png'
 
 export const ProductCard = ({product}: {product: ProductData}) => {
 
+    const [imageLoading, setImageLoading] = useState(true)
     const navigate = useNavigate()
 
     return <div className='ProductCard' onClick={()=>navigate(`/products/${product.id}`)}>
         <div className='inner'>
             <div className='image-container'>
-                <img className='image' src={product.imageUrl || 'fgdiyhuyfdfuygdfuydsuygusfyj'} alt={product.name} onError={e=>e.currentTarget.src=FALLBACK_IMG}/>
+                <img className='image' 
+                    style={imageLoading ? {display: 'none'} : {}} 
+                    src={product.imageUrl || 'fgdiyhuyfdfuygdfuydsuygusfyj'} 
+                    alt={product.name}
+                    onError={e=>e.currentTarget.src=FALLBACK_IMG}
+                    onLoad={()=>setImageLoading(false)}
+                />
+                <div className='lds-dual-ring' style={imageLoading ? {} : {display: 'none'}}></div>
             </div>
             <div className='info-container'>
                 <Price price={product.price}/>
